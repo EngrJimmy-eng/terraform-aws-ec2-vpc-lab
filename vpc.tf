03f338d0ab2dc2bfc38c4b67d034b3fbebbfffe7
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-02-02 17:49:21 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,19 +1,9 @@
-
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
-    Name = "public-rt"
+    Name = "terraform-vpc"
   }
-}
-
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
 }
