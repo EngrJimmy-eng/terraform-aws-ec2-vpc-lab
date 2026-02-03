2f9c23289731f063570d44293c66e414b1c63df6
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-02-03 11:55:46 +0100

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -12,3 +12,13 @@ resource "aws_iam_role" "ssm_role" {
     }]
   })
 }
+
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "ec2-ssm-profile"
+  role = aws_iam_role.ssm_role.name
+}
