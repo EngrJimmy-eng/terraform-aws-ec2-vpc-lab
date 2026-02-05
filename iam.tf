0abbb6e0285ae9d6b82ca1c462b794f4f528b25c
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-02-05 23:53:22 +0100

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ssm_role" {
-  name = "terraform-ec2-ssm-role-v2"
+  name = "terraform-ec2-ssm-role-clean"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
